d32ac9d2a28ec650955675bffcae5484727b3dd5
Tom Vossler <tom.vossler@gmail.com>
2021-06-18 09:07:44 -0400

    first push

---
+++ PyBank/main.py
@@ -0,0 +1,6 @@
+# Calculate the total number of months included in dataset
+# Calculate the net total amount of profit/losses over the entire period
+# Calculate the average of changes in profit/losses 
+# Calculate the greatest increase in profits (date and amount) over the entire period.
+# Calculate the greatest decrease in losses (date and amount over the entire period
+# 
---
+++ PyPoll/main.py
@@ -0,0 +1,5 @@
+# Calculate total number of votes
+# Create a complete list of candidates who received votes
+# Calculate the percentage of votes each candidate won
+# Caculate the total number of votes each candidate won
+# Determine the winner of the election based on popular vote
